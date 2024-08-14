5eeb6536fb78b24809c4e27ba7b0c7248ef601c6
Samraj10 <samraj2224@gmail.com>
2024-08-14 20:58:56 +0530

    adas

---
+++ gitrefresh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git_url="https://github.com/Samraj10/inquiryms.git"
+
+cd D:/applications/inquiryms
+
+git pull origin master 
--- src/main/java/com/infy/pr/inquiryms/InquiryApplication.java
+++ src/main/java/com/infy/pr/inquiryms/InquiryApplication.java
@@ -10,7 +10,6 @@ public class InquiryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InquiryApplication.class, args);
-		
 
 	}
 
